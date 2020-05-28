3d04667fbae6694ff41f8b2b7d6e7c7ef6244f77
Emanuel Rylke <ema@mailbox.org>
2020-05-28 18:48:27 +0200

    Have multi char choices

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let out_text = '';
 let n_choices = 2;
 let score = 0;
 let run = 0;
+const n_stride = 5;
+const truncate_at = 20;
 
 function run_score() {
     return Math.pow(Math.max(0, run - 2), 2);
@@ -14,38 +16,50 @@ function add_run() {
     let run_el = document.querySelector('#run');
     score_el.innerText = score
     run_el.innerText = run_score();
-
-
 }
 
+function update_label (b) {
+    b.innerText = b.c.replace(/^ | $/g, '_').replace(/^\n|\n$/g, '\\n');
+}
+
+function move(n) {
+    let foo = text.slice(0, n);
+    out_text += foo;
+    let out = document.querySelector('#output');
+    out.innerText = out_text + '|';
+    text = text.slice(n);
+
+    n_choices += n * 0.1;
+
+    run += n;
+    if (run_score() >= 100) {
+        add_run()
+    } else {
+        let run_el = document.querySelector('#run');
+        run_el.innerText = run_score();
+    }
+}
+
+function fail(b) {
+    add_run();
+    n_choices = Math.max(2, n_choices - 1);
+    b.classList.add('disabled');
+}
 
 function create_choice(c) {
     let button = document.createElement('div');
     button.className = 'choice';
     button.c = c;
-    button.innerText = c == ' ' ? '_' : c == '\n' ? '\\n' : c;
+    update_label(button);
     button.addEventListener('click', () => {
         if (button.classList.contains('disabled')) {
             return;
         }
-        if (c == text[0]) {
-            run++;
-            if (run_score() >= 100) {
-                add_run()
-            } else {
-                let run_el = document.querySelector('#run');
-                run_el.innerText = run_score();
-            }
-            n_choices += 0.1;
-            let out = document.querySelector('#output');
-            out_text += c;
-            out.innerText = out_text + '|';
-            text = text.slice(1);
+        if (text.startsWith(button.c)) {
+           move(button.c.length);
             show_choices();
         } else {
-            add_run();
-            n_choices = Math.max(2, n_choices - 1);
-            button.classList.add('disabled');
+            fail(button);
         }
     });
     return button;
@@ -55,7 +69,18 @@ function show_choices() {
     let choi = document.querySelector('#choices');
     choi.innerHTML = '';
 
-    let choices = [...new Set(text.slice(0, n_choices | 0))];
+    let choices = [];
+
+    let stride = Math.max(1, Math.min(5, n_stride * truncate_at / n_choices)) | 0;
+
+    for (let i = 0; i < n_choices; i++) {
+        let foo = text.slice(i * stride, (i + 1) * stride);
+        if (foo) {
+            choices.push(foo);
+        }
+    }
+
+    choices = [...new Set(choices)];
     choices.sort();
 
     if (!choices.length) {
@@ -70,7 +95,7 @@ function show_choices() {
 
 async function get_text() {
     let x = await fetch((document.location.hash.slice(1) || 'osterspaziergang') + '.txt');
-    return x.text();
+    return await x.text();
 }
 
 text_promise = get_text();
@@ -85,9 +110,26 @@ addEventListener('DOMContentLoaded', () => {
         if (key == 'Enter') {
             key = '\n';
         }
-        for (b of document.querySelectorAll('.choice')) {
-            if (b.c == key) {
-                b.click();
+        if (text[0] == key) {
+            move(1);
+            for (b of document.querySelectorAll('.choice')) {
+                if (b.c[0] == key && !b.classList.contains('disabled')) {
+                    event.preventDefault();
+                    b.c = b.c.slice(1);
+                    update_label(b);
+                    if (!b.c) {
+                        show_choices();
+                    }
+                } else {
+                    b.classList.add('disabled');
+                }
+            }
+        } else {
+            for (b of document.querySelectorAll('.choice')) {
+                if (b.c[0] == key && !b.classList.contains('disabled')) {
+                    event.preventDefault();
+                    fail(b);
+                }
             }
         }
     });
